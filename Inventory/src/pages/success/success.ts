f66acf9abaf964c4d108d0cfce2fb6feec42883c
testhackgame01 <41712108+testhackgame01@users.noreply.github.com>
2018-09-27 08:30:35 +0700

    Add files via upload

---
+++ Inventory/src/pages/success/success.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the SuccessPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-success',
+  templateUrl: 'success.html',
+})
+export class SuccessPage {
+  result
+  credential ="";
+
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SuccessPage');
+    this.credential = this.navParams.get("d555");
+  }
+
+}
