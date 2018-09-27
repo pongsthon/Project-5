92e3472d207df3b55d5eedb91ce9aec01e271f94
testhackgame01 <41712108+testhackgame01@users.noreply.github.com>
2018-09-27 09:23:09 +0700

    Add files via upload

--- Inventory/src/pages/success/success.ts
+++ Inventory/src/pages/success/success.ts
@@ -14,16 +14,96 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'success.html',
 })
 export class SuccessPage {
-  result
-  credential ="";
+// input2         input3            Output
+id11011_02    ;   id11011_03    ;   id11011_04;
+id11079_02    ;   id11079_03    ;   id11079_04;
+id11083_02    ;   id11083_03    ;   id11083_04;
+id11089_02    ;   id11089_03    ;   id11089_04;
+id11093_02    ;   id11093_03    ;   id11093_04;
+id11095_02    ;   id11095_03    ;   id11095_04;
+id11113_02    ;   id11113_03    ;   id11113_04;
+id11114_02    ;   id11114_03    ;   id11114_04;
+id11115_02    ;   id11115_03    ;   id11115_04;
+id11116_02    ;   id11116_03    ;   id11116_04;
+id11120_02    ;   id11120_03    ;   id11120_04;
+id11135_02    ;   id11135_03    ;   id11135_04;
+id11137_02    ;   id11137_03    ;   id11137_04;
+id11138_02    ;   id11138_03    ;   id11138_04;
+id11146_02    ;   id11146_03    ;   id11146_04;
+id11148_02    ;   id11148_03    ;   id11148_04;
+id11149_02    ;   id11149_03    ;   id11149_04;
+id11172_02    ;   id11172_03    ;   id11172_04;
+id11175_02    ;   id11175_03    ;   id11175_04;
+id11178_02    ;   id11178_03    ;   id11178_04;
+id11179_02    ;   id11179_03    ;   id11179_04;
+id11180_02    ;   id11180_03    ;   id11180_04;
+id11181_02    ;   id11181_03    ;   id11181_04;
+id11182_02    ;   id11182_03    ;   id11182_04;
+id11183_02    ;   id11183_03    ;   id11183_04;
+id11184_02    ;   id11184_03    ;   id11184_04;
+id11185_02    ;   id11185_03    ;   id11185_04;
+id11196_02    ;   id11196_03    ;   id11196_04;
 
-  constructor(public navCtrl: NavController,
-     public navParams: NavParams) {
-  }
+id12012_02    ;   id12012_03    ;   id12012_04;
+id12016_02    ;   id12016_03    ;   id12016_04;
+id12017_02    ;   id12017_03    ;   id12017_04;
+id12028_02    ;   id12028_03    ;   id12028_04;
+id12030_02    ;   id12030_03    ;   id12030_04;
+id12033_02    ;   id12033_03    ;   id12033_04;
+id12038_02    ;   id12038_03    ;   id12038_04;
+id21123_02    ;   id21123_03    ;   id21123_04;
+id21125_02    ;   id21125_03    ;   id21125_04;
+id21150_02    ;   id21150_03    ;   id21150_04;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SuccessPage');
-    this.credential = this.navParams.get("d555");
-  }
 
+
+
+
+constructor(public navCtrl: NavController) {
+
+}
+
+onClick(){
+this.id11011_04 = (parseInt(this.id11011_02)*26) + parseInt(this.id11011_03) ;
+this.id11079_04 = (parseInt(this.id11079_02)*16) + parseInt(this.id11079_03) ;
+this.id11083_04 = (parseInt(this.id11083_02)*31) + parseInt(this.id11083_03) ;
+this.id11089_04 = (parseInt(this.id11089_02)*20) + parseInt(this.id11089_03) ;
+this.id11093_04 = (parseInt(this.id11093_02)*12) + parseInt(this.id11093_03) ;
+this.id11095_04 = (parseInt(this.id11095_02)*12) + parseInt(this.id11095_03) ;
+this.id11113_04 = (parseInt(this.id11113_02)*31) + parseInt(this.id11113_03) ;
+this.id11114_04 = (parseInt(this.id11114_02)*60) + parseInt(this.id11114_03) ;
+this.id11115_04 = (parseInt(this.id11115_02)*60) + parseInt(this.id11115_03) ;
+this.id11116_04 = (parseInt(this.id11116_02)*27) + parseInt(this.id11116_03) ;
+this.id11120_04 = (parseInt(this.id11120_02)*48) + parseInt(this.id11120_03) ;
+this.id11135_04 = (parseInt(this.id11135_02)*32) + parseInt(this.id11135_03) ;
+this.id11137_04 = (parseInt(this.id11137_02)*45) + parseInt(this.id11137_03) ;
+this.id11138_04 = (parseInt(this.id11138_02)*1 ) + parseInt(this.id11138_03) ;
+this.id11146_04 = (parseInt(this.id11146_02)*5 ) + parseInt(this.id11146_03) ;
+this.id11148_04 = (parseInt(this.id11148_02)*1 ) + parseInt(this.id11148_03) ;
+this.id11149_04 = (parseInt(this.id11149_02)*10) + parseInt(this.id11149_03) ;
+this.id11172_04 = (parseInt(this.id11172_02)*12) + parseInt(this.id11172_03) ;
+this.id11175_04 = (parseInt(this.id11175_02)*1 ) + parseInt(this.id11175_03) ;
+this.id11178_04 = (parseInt(this.id11178_02)*16) + parseInt(this.id11178_03) ;
+this.id11179_04 = (parseInt(this.id11179_02)*40) + parseInt(this.id11179_03) ;
+this.id11180_04 = (parseInt(this.id11180_02)*27) + parseInt(this.id11180_03) ;
+this.id11181_04 = (parseInt(this.id11181_02)*16) + parseInt(this.id11181_03) ;
+this.id11182_04 = (parseInt(this.id11182_02)*40) + parseInt(this.id11182_03) ;
+this.id11183_04 = (parseInt(this.id11183_02)*20) + parseInt(this.id11183_03) ;
+this.id11184_04 = (parseInt(this.id11184_02)*27) + parseInt(this.id11184_03) ;
+this.id11185_04 = (parseInt(this.id11185_02)*27) + parseInt(this.id11185_03) ;
+this.id11196_04 = (parseInt(this.id11196_02)*60) + parseInt(this.id11196_03) ;
+
+this.id12012_04 = (parseInt(this.id12012_02)*6) + (parseInt(this.id12012_03)*2.64) ;
+this.id12016_04 = (parseInt(this.id12016_02)*12) + parseInt(this.id12016_03) ;
+this.id12017_04 = (parseInt(this.id12017_02)*6) + (parseInt(this.id12017_03)*2.64) ;
+this.id12028_04 = (parseInt(this.id12028_02)*20) + parseInt(this.id12028_03) ;
+this.id12030_04 = (parseInt(this.id12030_02)*15) + parseInt(this.id12030_03) ;
+this.id12033_04 = (parseInt(this.id12033_02)*45) + parseInt(this.id12033_03) ;
+this.id12038_04 = (parseInt(this.id12038_02)*20) + parseInt(this.id12038_03) ;
+this.id21123_04 = (parseInt(this.id21123_02)*1) + (parseInt(this.id21123_03)/12) ;
+this.id21125_04 = (parseInt(this.id21125_02)*30) + parseInt(this.id21125_03) ;
+this.id21150_04 = (parseInt(this.id21150_02)*25) + parseInt(this.id21150_03) ;
+
+
+}
 }
